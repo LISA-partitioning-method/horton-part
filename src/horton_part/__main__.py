d3e68c68a38574f7bac4828bbbbbeab41210aba7
YingXing <yingxing.cheng@mathematik.uni-stuttgart.de>
2023-08-14 09:01:35 +0200

    update header

--- src/horton_part/__main__.py
+++ src/horton_part/__main__.py
@@ -1,20 +1,22 @@
-# DensPart performs Atoms-in-molecules density partitioning.
-# Copyright (C) 2011-2020 The DensPart Development Team
+# -*- coding: utf-8 -*-
+# HORTON-PART: GRID for Helpful Open-source Research TOol for N-fermion systems.
+# Copyright (C) 2011-2023 The HORTON-PART Development Team
 #
-# This file is part of DensPart.
+# This file is part of HORTON-PART
 #
-# DensPart is free software; you can redistribute it and/or
+# HORTON-PART is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 3
 # of the License, or (at your option) any later version.
 #
-# DensPart is distributed in the hope that it will be useful,
+# HORTON-PART is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>
+#
 # --
 """Prepare inputs for denspart with HORTON3 modules.
 
